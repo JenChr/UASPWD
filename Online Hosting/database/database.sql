b25d05906554b9dc90c143eb4fcf3ca76962a481
Dyoline <161908369+Dyoline@users.noreply.github.com>
2024-06-24 23:54:42 +0700

    Add files via upload

---
+++ Online Hosting/database/database.sql
@@ -0,0 +1,66 @@
+-- CREATE DATABASE IF NOT EXISTS sql12714626;
+-- USE sql12714626;
+
+-- DROP TABLE IF EXISTS tangki, pipa, sambungan, bahan_saringan;
+
+-- -- TABLE TANGKI
+-- CREATE TABLE tangki (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(255) NOT NULL,
+--   img VARCHAR(255) NOT NULL
+--  );
+ 
+-- INSERT INTO tangki (name, img) VALUES
+-- ('TB 350', 'assets/product/350.png'),
+-- ('TB 400', 'assets/product/400.png'),
+-- ('TB 550', 'assets/product/550.png'),
+-- ('TB 600', 'assets/product/600.png'),
+-- ('TB 800', 'assets/product/800.png'),
+-- ('TB 900', 'assets/product/900.png'),
+-- ('TB 1100', 'assets/product/1100.png'),
+-- ('TB 1101S', 'assets/product/1101S.png'),
+-- ('TB 1150', 'assets/product/1150.png'),
+-- ('TB 2200', 'assets/product/2200.png'),
+-- ('TB 2500', 'assets/product/2500.png'),
+-- ('TB 3300', 'assets/product/3300.png'),
+-- ('TB 3500', 'assets/product/3500.png'),
+-- ('TB 5500', 'assets/product/5500.png');
+
+-- -- TABLE PIPA
+--  CREATE TABLE pipa (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     img VARCHAR(255) NOT NULL
+--  );
+
+-- INSERT INTO pipa (name, img) VALUES
+-- ('Rucika AW', 'assets/product/Rucika AW.png'),
+-- ('Rucika D', 'assets/product/Rucika D.png');
+
+-- -- TABLE SAMBUNGAN
+--  CREATE TABLE sambungan (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     img VARCHAR(255) NOT NULL
+--  );
+
+-- INSERT INTO sambungan (name, img) VALUES
+-- ('Elbow Out (45°)', 'assets/product/45 derajat elbow_out.png'),
+-- ('Elbow Out', 'assets/product/elbow_out.png'),
+-- ('Reducing Socket Out', 'assets/product/Reducing socket_out.png'),
+-- ('Socket Out', 'assets/product/socket_out.png'),
+-- ('Tee', 'assets/product/Tee.png'),
+-- ('Valve Socket Out', 'assets/product/valve socket_out.png');
+
+-- -- TABLE BAHAN SARINGAN
+--  CREATE TABLE bahan_saringan (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     img VARCHAR(255) NOT NULL
+--  );
+
+-- INSERT INTO bahan_saringan (name, img) VALUES
+-- ('Batu', 'assets/product/batu.png'),
+-- ('Pasir Kerang', 'assets/product/pasir.png'),
+-- ('Ijuk', 'assets/product/ijuk.png'),
+-- ('Arang', 'assets/product/arang.png');
---
+++ Online Hosting/database/db.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE IF NOT EXISTS sql12715720;
+USE sql12715720;
+
+DROP TABLE IF EXISTS tangki, pipa, sambungan, bahan_saringan;
+
+-- TABLE TANGKI
+CREATE TABLE tangki (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  img VARCHAR(255) NOT NULL
+ );
+ 
+INSERT INTO tangki (name, img) VALUES
+('TB 350', '350.png'),
+('TB 400', '400.png'),
+('TB 550', '550.png'),
+('TB 600', '600.png'),
+('TB 800', '800.png'),
+('TB 900', '900.png'),
+('TB 1100', '1100.png'),
+('TB 1101S', '1101S.png'),
+('TB 1150', '1150.png'),
+('TB 2200', '2200.png'),
+('TB 2500', '2500.png'),
+('TB 3300', '3300.png'),
+('TB 3500', '3500.png'),
+('TB 5500', '5500.png');
+
+-- TABLE PIPA
+ CREATE TABLE pipa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    img VARCHAR(255) NOT NULL
+ );
+
+INSERT INTO pipa (name, img) VALUES
+('Rucika AW', 'Rucika AW.png'),
+('Rucika D', 'Rucika D.png');
+
+-- TABLE SAMBUNGAN
+ CREATE TABLE sambungan (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    img VARCHAR(255) NOT NULL
+ );
+
+INSERT INTO sambungan (name, img) VALUES
+('Elbow Out (45°)', '45 derajat elbow_out.png'),
+('Elbow Out', 'elbow_out.png'),
+('Reducing Socket Out', 'Reducing socket_out.png'),
+('Socket Out', 'socket_out.png'),
+('Tee', 'Tee.png'),
+('Valve Socket Out', 'valve socket_out.png');
+
+-- TABLE BAHAN SARINGAN
+ CREATE TABLE bahan_saringan (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    img VARCHAR(255) NOT NULL
+ );
+
+INSERT INTO bahan_saringan (name, img) VALUES
+('Batu', 'batu.png'),
+('Pasir Kerang', 'pasir.png'),
+('Ijuk', 'ijuk.png'),
+('Arang', 'arang.png');
